d1cf2cc32071cb1ca8f49e7e6b785ac8f3a6003f
MarySerna <mary.serna.8903@gmail.com>
2021-01-30 03:25:40 +0000

    First API

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,14 +1,12 @@
 const redisClient = require('../utils/redis');
 const dbClient = require('../utils/db');
 
-/** getStatus - callback for route GET /status*/
 function getStatus(req, res) {
   if (redisClient.isAlive() && dbClient.isAlive()) {
     res.status(200).json({ redis: true, db: true });
   }
 }
 
-/** getStats - callback for route GET /stats*/
 async function getStats(req, res) {
   const userNum = await dbClient.nbUsers();
   const fileNum = await dbClient.nbFiles();
